e74508744049730808c9656df9deb55c0a0fdf08
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-03-04 22:12:04 +0100

    heroku deploy

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://ecommercemustafa.now.sh"
+    ? "https://ecommercemustafa.herokuapp.com/"
     : "http://localhost:3000";
 
 export default baseUrl;
